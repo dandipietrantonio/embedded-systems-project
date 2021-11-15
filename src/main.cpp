81950edae6810400764b28c5ab8132a542c41be0
dandipietrantonio <dd2742@nyu.edu>
2021-11-15 18:44:13 -0500

    Quick guide to getting accelerometer data into a file

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
+#include <Adafruit_CircuitPlayground.h>
+
+float X, Y, Z;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  CircuitPlayground.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (CircuitPlayground.leftButton()) {
+    X = CircuitPlayground.motionX();
+    Y = CircuitPlayground.motionY();
+    Z = CircuitPlayground.motionZ();
+
+    Serial.print('(');
+    Serial.print(X);
+    Serial.print(',');
+    Serial.print(Y);
+    Serial.print(',');
+    Serial.print(Z);
+    Serial.print("); ");
+  }
+
+  delay(10);
 }
