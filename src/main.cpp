e3481b1eb53ba538526c7537b03aa667fa7d0c61
dandipietrantonio <dd2742@nyu.edu>
2021-12-08 18:44:00 -0500

    First try with new modular attempt

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,78 @@
 #include <Arduino.h>
+#include <Adafruit_CircuitPlayground.h>
+
+/*
+  The idea is to decompose the motion into three sub movements. Each submovement is identified
+  by the axis that experiences the most acceleration during that movement. Once we have three 
+  movements, that is our message.
+
+  NOTE: board must be held as flat as possible, with buttons facing up
+*/
+
+struct XYZ {
+  float x;
+  float y;
+  float z;
+};
+
+int CUR_MOVEMENT= 1; // the current motion we're on
+XYZ CUR_MOVEMENT_ARR[300]; // you have three seconds to make your movement
+int NUM_READINGS_FOR_CUR_MOVEMENT=0;
+
+float X,Y,Z; // current accelerometer reading
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  CircuitPlayground.begin();
+}
+
+void classifyMovement() {
+  int xSum, ySum, zSum;
+  for (int i = 0; i < NUM_READINGS_FOR_CUR_MOVEMENT; i++) {
+    XYZ curReading = CUR_MOVEMENT_ARR[i];
+    float curX = curReading.x;
+    float curY = curReading.y;
+    float curZ = curReading.z - 9.8; // to account for gravity
+
+    xSum += curX > 0 ? curX : -1 * curX;
+    ySum += curY > 0 ? curY : -1 * curY;
+    zSum += curZ > 0 ? curZ : -1 * curZ;
+  }
+
+  if (xSum >= ySum && xSum >= zSum) {
+    Serial.println("X is max!");
+  } else if (ySum >= xSum && ySum >= zSum) {
+    Serial.println("Y is max!");
+  } else if (zSum >= xSum && zSum >= ySum) {
+    Serial.println("Y is max!");
+  }
+}
+
+void tooManyReadingsError(){
+  Serial.println("ERROR: You took too long, you only have 3 seconds to record your movement");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  X = CircuitPlayground.motionX();
+  Y = CircuitPlayground.motionY();
+  Z = CircuitPlayground.motionZ();
+
+  bool needToClassify = false;
+  while (CircuitPlayground.leftButton()) {
+    needToClassify = true;
+    if (NUM_READINGS_FOR_CUR_MOVEMENT == 300) {
+      tooManyReadingsError();
+    } else {
+      XYZ curReading;
+      curReading.x = CircuitPlayground.motionX();
+      curReading.y = CircuitPlayground.motionY();
+      curReading.z = CircuitPlayground.motionZ();
+      CUR_MOVEMENT_ARR[NUM_READINGS_FOR_CUR_MOVEMENT] = curReading;
+
+      NUM_READINGS_FOR_CUR_MOVEMENT += 1;
+    }
+  }
+  if (needToClassify) classifyMovement();
+
+  delay(10);
 }
